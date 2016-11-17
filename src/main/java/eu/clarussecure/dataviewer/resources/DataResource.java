3b1fbde4f31b5ec8e92aa1adccc1ecb004ba4544
Thierry Chevallier <thierry.chevallier@akka.eu>
2016-11-17 10:47:49 +0100

    JSON representation of SQL results

--- src/main/java/eu/clarussecure/dataviewer/resources/DataResource.java
+++ src/main/java/eu/clarussecure/dataviewer/resources/DataResource.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package eu.clarussecure.dataviewer.resources;
 
 import com.google.gson.Gson;
@@ -11,12 +6,16 @@ import eu.clarussecure.dataviewer.model.SecurityPolicyEndpoint;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -25,9 +24,38 @@ import org.springframework.web.client.RestTemplate;
 @Path("/data")
 public class DataResource {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    /**
+     * getPSQLData
+     * @param tableName
+     * @return
+     * @throws Exception
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/pgsql/{table}")
+    public String getPSQLData(@PathParam("table") String tableName) throws Exception {
+
+        /*TODO prevent SQL injection ---> check that tableName is in security policy attributes ? */
+        String sql = String.format("SELECT * FROM %s", tableName);
+        JSONArray json = new JSONArray();
+        
+        List<Map<String,Object>> results = jdbcTemplate.queryForList(sql);
+        results.forEach((result) -> {
+            JSONObject obj = new JSONObject();
+            result.entrySet().forEach((entry) -> {
+                obj.put(entry.getKey(), entry.getValue());
+            });
+            json.add(obj);
+        });
+        
+        return json.toString();
+    }
+
     /**
      * getWFSData
-     *
      * @param layerName
      * @return
      * @throws Exception
